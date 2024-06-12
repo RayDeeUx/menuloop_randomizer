cac95adb0a30c8bc3ab72a8ce16b53509236e336
elnexreal <40328573+elnexreal@users.noreply.github.com>
2024-06-11 21:50:13 -0300

    initial commit

---
+++ about.md
@@ -0,0 +1,14 @@
+# Menu Loop Randomizer
+
+Are you bored of hearing the same menu song <cr>over & over</cr>? Well... **No more of that!**
+
+This mod allows you to change the menu song every time you open the game, picking from the songs you downloaded through Newgrounds.
+
+# Notes
+
+- The song changes every time you restart the game, so you will not get another song every time you go back to the menu.
+- In the future i'll add a setting to toggle between using NG songs or your own songs.
+
+# Thanks to!
+
+- [hiimjustin000](https://github.com/hiimjustin000) <cj>for helping me with the bindings problem (i was using an outdated version of geode LMAO)</cj>
---
+++ src/main.cpp
@@ -0,0 +1,37 @@
+#include <Geode/Geode.hpp>
+#include <Geode/modify/GameManager.hpp>
+#include <random>
+#include <vector>
+
+using namespace geode::prelude;
+
+std::string selectedSong;
+
+$execute {
+	std::vector<std::string> songs;
+
+	// get the path for the songs
+	std::filesystem::path ngSongsPath = CCFileUtils::get()->getWritablePath();
+
+	// add all the mp3 files to the vector
+	for (auto &song : std::filesystem::directory_iterator(ngSongsPath)) {
+		if (song.path().string().ends_with(".mp3"))
+			songs.push_back(song.path().string());
+	}
+
+	// select a random item from the vector and return the path
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	std::uniform_int_distribution<> dist(0, songs.size() - 1);
+	int randomIndex = dist(gen);
+
+	selectedSong = songs[randomIndex];
+}
+
+struct GameManagerHook : Modify<GameManagerHook, GameManager> {
+	gd::string getMenuMusicFile() {
+		// return GameManager::getMenuMusicFile();
+
+		return selectedSong;
+	}
+};
