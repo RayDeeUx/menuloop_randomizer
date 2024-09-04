88ecaf2b6f1f3550e148ba9fd953985c0b9f0661
Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2024-09-04 12:09:12 -0400

    for those installing from a fresh profile
    Signed-off-by: Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>

--- src/main.cpp
+++ src/main.cpp
@@ -97,6 +97,7 @@ $execute {
 		Utils::setNewSong();
 	});
 	listenForSettingChanges<bool>("playlistMode", [](bool isPlaylistMode) {
+		if (SongManager::get().isOriginalMenuLoop()) return;
 		FMODAudioEngine::get()->m_backgroundMusicChannel->stop();
 		if (isPlaylistMode) {
 			return FMODAudioEngine::get()->playMusic(SongManager::get().getCurrentSong(), true, 1.0f, 1);
