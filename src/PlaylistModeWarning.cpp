9fa7313c7aaf046205caa3f6db1d00e4820214fa
Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2025-01-14 20:58:37 -0500

    edit desc
    Signed-off-by: Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>

--- src/PlaylistModeWarning.cpp
+++ src/PlaylistModeWarning.cpp
@@ -16,7 +16,7 @@ bool PlaylistModeWarning::init(bool isCringe) {
 	if (!FLAlertLayer::init(nullptr, "Playlist Mode Warning",
 		fmt::format(
 			"{}<cr>This feature is ***very*** unstable. It's best if you restart your game after toggling this setting.</c>\n\n<cy>Known issues (usually for non-Windows users):</c>\n<cy>- Songs may stop changing while/after viewing Geode UI, the Tower, the icon kit, or the shops.</c>\n<cy>- Playlist Mode disables loops from the Tower, all shops, and all vaults. (Not much of a loss; those loops get tiring after a while.)</c>",
-			isCringe ? "<cy>Geodify's \"Menu Loop\" option is enabled.</c> <cr></c>\n\n" : ""),
+			isCringe ? "<cy>Geodify's \"Menu Loop\" option is enabled.</c> <cr>Things may break.</c>\n\n" : ""),
 		"I Understand", nullptr, 420.f, true, 300.f, 1.f)) return false;
 	return true;
 }
