2695b453449f956ecf1de31aec8fb178569d86d4
Erymanthus | RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2025-12-09 19:31:01 -0500

    people keep asking me to do linesplits

--- src/FMODAudioEngine.cpp
+++ src/FMODAudioEngine.cpp
@@ -8,12 +8,10 @@ class $modify(MenuLoopFMODHook, FMODAudioEngine) {
 	void update(float dt) {
 		FMODAudioEngine::update(dt);
 		if (VANILLA_GD_MENU_LOOP_DISABLED) return;
+
 		SongManager& songManager = SongManager::get();
 		constexpr int channelNumber = 0;
-		bool isPlaying = true;
-		unsigned int position = 0;
-		FMOD::Sound* sound;
-		unsigned int length = 0;
+
 		FMOD::Channel* menuLoopChannelProbably = FMODAudioEngine::get()->getActiveMusicChannel(channelNumber);
 		const auto activeSong = FMODAudioEngine::get()->getActiveMusic(channelNumber);
 		const auto songManagerSong = songManager.getCurrentSong();
@@ -25,13 +23,21 @@ class $modify(MenuLoopFMODHook, FMODAudioEngine) {
 			}
 			return;
 		}
-		menuLoopChannelProbably->isPlaying(&isPlaying);
+
+		unsigned int position = 0;
 		menuLoopChannelProbably->getPosition(&position, 1);
-		menuLoopChannelProbably->getCurrentSound(&sound);
 		songManager.setLastMenuLoopPosition(position); // so the problem with trying to move this logic to GJBGL::init() is that by the time GJBGL::init() is called, FMOD's menu loop track has already been stopped and its position resets to 0. hence why we are here instead.
+
 		if (!songManager.getConstantShuffleMode() || songManager.isOverride()) return;
 		if (GJBaseGameLayer::get() || songManager.isOriginalMenuLoop() || songManager.getSongsSize() < 2) return;
+
+		bool isPlaying = true;
+		FMOD::Sound* sound;
+		unsigned int length = 0;
+		menuLoopChannelProbably->getCurrentSound(&sound);
+		menuLoopChannelProbably->isPlaying(&isPlaying);
 		sound->getLength(&length, 1);
+
 		if (SongManager::get().getAdvancedLogs()) {
 			log::info("isSongManagerSong: {}", isSongManagerSong);
 			log::info("position: {}", position);
@@ -40,6 +46,7 @@ class $modify(MenuLoopFMODHook, FMODAudioEngine) {
 			log::info("(length - 100) < position: {}", (length - 100) < position);
 			log::info("===========================");
 		}
+
 		if ((length - 100) < position) {
 			geode::log::info("song is probably finished. Switching songs.");
 			Utils::removeCard();
