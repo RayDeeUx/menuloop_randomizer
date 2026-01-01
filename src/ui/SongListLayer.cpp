063866f4d370c0995d95fc373c7609b006dac5c4
Erymanthus | RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2025-12-31 21:39:49 -0500

    ppl cant read

--- src/ui/SongListLayer.cpp
+++ src/ui/SongListLayer.cpp
@@ -280,11 +280,13 @@ bool SongListLayer::setup(const std::string&) {
 		"Yellow = <cy>favorited</c> song. Green = <cg>current</c> song.\n"
 		"Italicized = NG/ML song from unexpected path. Gradient = Jukebox (NONG).\n"
 		"You can <cl>shuffle songs</c>, <cl>copy the current song's name</c>,\n"
-		"<cl>go back one song</c>, and <cl>reopen the control panel</c>.\n\n"
+		"<cl>go back one song</c>, and <cl>reopen the control panel</c>.\n"
 		"<cy>Q: I'm seeing numbers for most of my songs?!</c>\n"
-		"A: Someone probably goofed up, which is out of my reach.\n\n"
+		"A: Someone probably goofed up, which is out of my reach.\n"
 		"<cy>Q: Make this mod act more closely to Spotify!</c>\n"
-		"A: <c_>No. Never.</c>",
+		"A: Someone probably goofed up, which is out of my reach.\n"
+		"<cy>Q: Where is the search bar and song sorting options?\n"
+		"A: <c_>Please install \"Better Touch Prio\" by alk1m123 and undefined0.</c>",
 		.75f
 	);
 	infoMenu->addChildAtPosition(infoBtn, geode::Anchor::Center);
