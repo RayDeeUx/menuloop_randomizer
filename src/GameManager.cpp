5206fc030ac98ce8c279eac5de3cf764f1ce94d3
Erymanthus | RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2026-01-02 09:42:51 -0500

    backup hook prio changes

--- src/GameManager.cpp
+++ src/GameManager.cpp
@@ -5,20 +5,36 @@
 using namespace geode::prelude;
 
 class $modify(MenuLoopGMHook, GameManager) {
+	static void onModify(auto& self) {
+		(void) self.setHookPriority("GameManger::getMenuMusicFile", -3999);
+		(void) self.setHookPriority("GameManger::playMenuMusic", -3999);
+		(void) self.setHookPriority("GameManger::fadeInMenuMusic", -3999);
+	}
 	gd::string getMenuMusicFile() {
 		return SongManager::get().getCurrentSong();
+	}
+	void playMenuMusic() {
+		// manual reconstruction
+		FMODAudioEngine* fmod = FMODAudioEngine::get();
+		if (VANILLA_GD_MENU_LOOP_DISABLED) return fmod->stopAllMusic(true);
+		if (!fmod->isMusicPlaying(GameManager::get()->getMenuMusicFile(), 0)) fmod->playMusic(GameManager::get()->getMenuMusicFile(), true, 0, 0);
 	}
 	void fadeInMenuMusic() {
 		// code taken verbatim from colon. i've had two too many toddlers nagging me to add this feature to the mod now so here we are
 		FMODAudioEngine* fmod = FMODAudioEngine::get();
+		if (VANILLA_GD_MENU_LOOP_DISABLED) return fmod->stopAllMusic(true);
+
 		SongManager& songManager = SongManager::get();
-		const auto oldTrack = fmod->getActiveMusic(0);
-		GameManager::fadeInMenuMusic();
+
 		// ensure compat with colon
 		const geode::Mod* colon = songManager.getColonMenuLoopStartTime();
-		if (colon && colon->getSettingValue<bool>("enable")) return;
-		if (!songManager.getShouldRestoreMenuLoopPoint()) return;
-		if (oldTrack == fmod->getActiveMusic(0)) return;
+		if (colon && colon->getSettingValue<bool>("enable")) return GameManager::fadeInMenuMusic();
+
+		// manual function reconstruction
+		const auto oldTrack = fmod->getActiveMusic(0);
+		if (!fmod->isMusicPlaying(GameManager::get()->getMenuMusicFile(), 0)) fmod->playMusic(GameManager::get()->getMenuMusicFile(), true, 1, 0);
+
+		if (!songManager.getShouldRestoreMenuLoopPoint() || oldTrack == fmod->getActiveMusic(0)) return;
 		songManager.restoreLastMenuLoopPosition();
 		songManager.setShouldRestoreMenuLoopPoint(false);
 	}
